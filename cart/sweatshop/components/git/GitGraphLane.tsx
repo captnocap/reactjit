72f7376e2059422324ac4ac4967e324590f2b32c
josiah <price.josiah95@gmail.com>
2026-04-22 09:20:47 -0700

    gitpanel-split: GitGraphLane component

---
+++ cart/sweatshop/components/git/GitGraphLane.tsx
@@ -0,0 +1,27 @@
+const React: any = require('react');
+
+import { Box, Text } from '../../../../runtime/primitives';
+import { COLORS } from '../../theme';
+
+interface GitGraphLaneProps {
+  graph: string;
+  color?: string;
+}
+
+export function GitGraphLane(props: GitGraphLaneProps) {
+  const color = props.color || COLORS.blue;
+  const chars = (props.graph || '').split('');
+
+  return (
+    <Box style={{ flexDirection: 'row', alignItems: 'center', gap: 0, paddingRight: 4 }}>
+      {chars.map((ch, i) => {
+        const tone = ch === '*' ? COLORS.textBright : color;
+        return (
+          <Text key={i} fontSize={10} color={tone} style={{ fontFamily: 'monospace', lineHeight: 14 }}>
+            {ch === ' ' ? '\u00A0' : ch}
+          </Text>
+        );
+      })}
+    </Box>
+  );
+}
