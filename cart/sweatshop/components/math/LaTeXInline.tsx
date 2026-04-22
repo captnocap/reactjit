e7e56a240d18e8d6ccce6b0e08b8440da8fc04f5
josiah <price.josiah95@gmail.com>
2026-04-22 09:46:16 -0700

    math: add inline latex

---
+++ cart/sweatshop/components/math/LaTeXInline.tsx
@@ -0,0 +1,25 @@
+const React: any = require('react');
+
+import { Box } from '../../../../runtime/primitives';
+import { useTheme } from '../../theme';
+import { renderMathTree } from './mathRender';
+import { useLaTeXParse } from './useLaTeXParse';
+
+export type LaTeXInlineProps = {
+  source: string;
+  fontSize?: number;
+  color?: string;
+  style?: any;
+};
+
+export function LaTeXInline({ source, fontSize = 16, color, style }: LaTeXInlineProps) {
+  const theme = useTheme();
+  const nodes = useLaTeXParse(source);
+  return (
+    <Box style={{ flexDirection: 'row', alignItems: 'flex-start', ...style }}>
+      {renderMathTree(nodes, { fontSize, color: color || theme.colors.text, inline: true })}
+    </Box>
+  );
+}
+
+export default LaTeXInline;
