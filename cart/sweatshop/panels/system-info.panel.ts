3824c6c060ebcbe3e98deeb7f75ae0e766281000
josiah <price.josiah95@gmail.com>
2026-04-22 10:54:02 -0700

    system-info: register panel

---
+++ cart/sweatshop/panels/system-info.panel.ts
@@ -0,0 +1,14 @@
+const React: any = require('react');
+
+import { SystemInfoPanel } from '../components/system-info/SystemInfoPanel';
+import { register } from '../panel-registry';
+
+register({
+  id: 'system-info',
+  title: 'System Info',
+  defaultSlot: 'right',
+  icon: 'terminal',
+  component: SystemInfoPanel,
+  userVisible: true,
+  defaultOpen: false,
+});
